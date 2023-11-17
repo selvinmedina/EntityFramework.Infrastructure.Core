c17aa6579028ecb87d1abd14360c4ac4a878df60
Selvin Medina <selvinmedina0016@gmail.com>
2023-11-17 11:09:29 -0600

    Solventando error de llamada a generico

--- EntityFramework.Infrastructure.Core/Extensions/ModelBuilderExtensions.cs
+++ EntityFramework.Infrastructure.Core/Extensions/ModelBuilderExtensions.cs
@@ -4,15 +4,61 @@ namespace SelvinMedina.EntityFramework.Infrastructure.Core.Extensions
 {
     public static class ModelBuilderExtensions
     {
-        public static ModelBuilder AddConfiguration<TEntity>(
-          this ModelBuilder modelBuilder) where TEntity : class
+        public static ModelBuilder AddConfiguration<TConfig>(this ModelBuilder modelBuilder)
+            where TConfig : class, new()
         {
-            var entityMap = (IEntityTypeConfiguration<>)Activator.CreateInstance(typeof(TEntity))!;
-            var configureMethod = entityMap.GetType().GetMethod("Configure")!;
+            if (modelBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(modelBuilder), "ModelBuilder cannot be null.");
+            }
 
-            modelBuilder.Entity<TEntity>(configureMethod.Invoke();
+            try
+            {
+                var configInterface = typeof(TConfig).GetInterfaces()
+                    .FirstOrDefault(i =>
+                        i.IsGenericType &&
+                        i.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>));
 
-            return modelBuilder;
+                if (configInterface == null)
+                {
+                    throw new InvalidOperationException("The provided class does not implement IEntityTypeConfiguration<TEntity>.");
+                }
+
+                var entityType = configInterface.GetGenericArguments()[0];
+                var configInstance = new TConfig();
+
+                var configureMethod = configInterface.GetMethod("Configure");
+                if (configureMethod == null)
+                {
+                    throw new InvalidOperationException("The Configure method was not found on the IEntityTypeConfiguration interface.");
+                }
+
+                var type = modelBuilder.GetType();
+                var genericEntityMethod = type.GetMethods()
+                    .FirstOrDefault(m => m.Name == "Entity" && m.IsGenericMethod && m.GetParameters().Length == 0);
+
+                if (genericEntityMethod == null)
+                {
+                    throw new InvalidOperationException("The Entity method was not found on the ModelBuilder class.");
+                }
+
+                var entityTypeBuilder = genericEntityMethod.MakeGenericMethod(entityType).Invoke(modelBuilder, null);
+                if (entityTypeBuilder == null)
+                {
+                    throw new InvalidOperationException("Failed to create an instance of EntityTypeBuilder.");
+                }
+
+                configureMethod.Invoke(configInstance, new[] { entityTypeBuilder });
+
+                return modelBuilder;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                // Consider rethrowing or handling the exception based on your use case
+                throw;
+            }
         }
     }
 }
